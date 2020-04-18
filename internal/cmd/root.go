42328b21ef6ac7fe113afb832ea4b271675acd51
Ales Nosek <ales.nosek@gmail.com>
2020-04-18 15:06:31 -0700

    Improve command help

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -11,16 +11,16 @@ func RootCmd() *cobra.Command {
 	var rootCmd = &cobra.Command{
 		Use:   "kube-debug-pod POD",
 		Short: "Attach troubleshooting tools to running Kubernetes/OpenShift pods",
-		Long:  `Complete documentation is available at https://github.com/noseka1/kube-debug-pod`,
+		Long:  `kube-debug-pod, complete documentation is available at https://github.com/noseka1/kube-debug-pod`,
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			params.pod = args[0]
 			NewDebugCmd(params).Execute()
 		},
 	}
-	rootCmd.PersistentFlags().StringVarP(&params.namespace, "namespace", "n", "", "Debug pod in namespace")
-	rootCmd.PersistentFlags().StringVarP(&params.container, "container", "c", "", "Debug container name; defaults to first container in pod")
-	rootCmd.PersistentFlags().BoolVarP(&params.initContainer, "init-container", "", false, "Debug an init container; defaults to false")
+	rootCmd.PersistentFlags().StringVarP(&params.namespace, "namespace", "n", "", "Target namespace")
+	rootCmd.PersistentFlags().StringVarP(&params.container, "container", "c", "", "Target container name; defaults to first container in pod")
+	rootCmd.PersistentFlags().BoolVarP(&params.initContainer, "init-container", "", false, "Target is an init container; defaults to false")
 	rootCmd.PersistentFlags().StringVarP(&params.image, "image", "", "centos", "Image used by the debug pod.")
 	return rootCmd
 }
