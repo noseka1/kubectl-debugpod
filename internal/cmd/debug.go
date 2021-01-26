a1236228c7de17caa1d6698a577e0ffa7237cd6b
Ales Nosek <ales.nosek@gmail.com>
2021-01-25 19:57:16 -0800

    Rename mount point /rootfs -> /host

--- internal/cmd/debug.go
+++ internal/cmd/debug.go
@@ -34,8 +34,8 @@ set -o xtrace
 CRI_PROVIDER="$1"
 CRI_ID="$2"
 CRI_SOCKET="$3"
-ROOTFS=/rootfs
-chroot $ROOTFS \
+HOSTFS=/host
+chroot $HOSTFS \
   crictl \
     --runtime-endpoint "$CRI_SOCKET" \
 	info
@@ -44,7 +44,7 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 INDENT=$(
-  chroot $ROOTFS \
+  chroot $HOSTFS \
     crictl \
       --runtime-endpoint "$CRI_SOCKET" \
 	  inspect \
@@ -54,7 +54,7 @@ INDENT=$(
   | sed --quiet --expression '1s/^\( \+\).*/\1/p'
 )
 PID=$(
-  chroot $ROOTFS \
+  chroot $HOSTFS \
     crictl \
       --runtime-endpoint "$CRI_SOCKET" \
 	  inspect \
@@ -64,7 +64,7 @@ PID=$(
 )
 if [ -z "$PID" ]; then
   # assume dockershim runtime, cri id equals to docker container id
-  PID=$(chroot /rootfs docker inspect --format '{{.State.Pid}}' "$CRI_ID")
+  PID=$(chroot $HOSTFS docker inspect --format '{{.State.Pid}}' "$CRI_ID")
 fi
 if [ -z "$PID" ]; then
   echo Failed to find the process PID for pod with CRI_ID=$CRI_ID
@@ -74,21 +74,21 @@ fi
 mkdir -p /kube-debug-pod
 cat >/kube-debug-pod/print_root.sh <<EOF
 #!/bin/sh
-ROOTFS=$ROOTFS
+HOSTFS=$HOSTFS
 CRI_ID=$CRI_ID
 CRI_PROVIDER=$CRI_PROVIDER
 if [ \$CRI_PROVIDER = docker ]; then
-  ROOT=\$(chroot \$ROOTFS docker inspect --format '{{.GraphDriver.Data.MergedDir}}' "\$CRI_ID")
+  ROOT=\$(chroot \$HOSTFS docker inspect --format '{{.GraphDriver.Data.MergedDir}}' "\$CRI_ID")
 elif [ \$CRI_PROVIDER = containerd ]; then
   ROOT=/run/containerd/io.containerd.runtime.v1.linux/k8s.io/\$CRI_ID/rootfs
 elif [ \$CRI_PROVIDER = cri-o ]; then
-  ROOT=\$(chroot \$ROOTFS runc state \$CRI_ID | sed --quiet --expression 's/ \+"rootfs": "\(.*\)".*/\1/p')
+  ROOT=\$(chroot \$HOSTFS runc state \$CRI_ID | sed --quiet --expression 's/ \+"rootfs": "\(.*\)".*/\1/p')
 fi
-if [ -z "\$ROOT" -o ! -d "\$ROOTFS\$ROOT" ]; then
+if [ -z "\$ROOT" -o ! -d "\$HOSTFS\$ROOT" ]; then
   echo Failed to obtain the root directory
   exit 1
 fi
-echo \$ROOTFS\$ROOT
+echo \$HOSTFS\$ROOT
 EOF
 chmod 755 /kube-debug-pod/print_root.sh
 
@@ -236,8 +236,8 @@ func (cmd *DebugCmd) prepareDebugPodManifest(node string, podName string, criPro
 						Privileged: pointer.BoolPtr(true),
 					},
 					VolumeMounts: []corev1.VolumeMount{{
-						Name:      "rootfs",
-						MountPath: "/rootfs",
+						Name:      "host",
+						MountPath: "/host",
 					}},
 				},
 			},
@@ -246,7 +246,7 @@ func (cmd *DebugCmd) prepareDebugPodManifest(node string, podName string, criPro
 			HostIPC:       true,
 			RestartPolicy: corev1.RestartPolicyNever,
 			Volumes: []corev1.Volume{{
-				Name:         "rootfs",
+				Name:         "host",
 				VolumeSource: corev1.VolumeSource{HostPath: &corev1.HostPathVolumeSource{Path: "/"}},
 			}},
 		},
