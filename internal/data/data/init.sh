5d026fea08e884c1fd6345d9965b8a735218fba6
Ales Nosek <ales.nosek@gmail.com>
2021-12-30 21:36:21 +0100

    Fix shell coding style

--- internal/data/data/init.sh
+++ internal/data/data/init.sh
@@ -2,65 +2,65 @@
 
 set -o xtrace
 
-CRI_PROVIDER="$1"
-CRI_ID="$2"
-CRI_SOCKET="$3"
+cri_provider="$1"
+cri_id="$2"
+cri_socket="$3"
 shift 3
-HOSTFS=/host
-chroot $HOSTFS \
+hostfs=/host
+chroot $hostfs \
   crictl \
-    --runtime-endpoint "$CRI_SOCKET" \
-	info
+    --runtime-endpoint "$cri_socket" \
+    info
 if [ $? -ne 0 ]; then
-  echo Failed to communicate with container runtime using socket $CRI_SOCKET
+  echo Failed to communicate with container runtime using socket $cri_socket
   exit 1
 fi
-INDENT=$(
-  chroot $HOSTFS \
+indent=$(
+  chroot $hostfs \
     crictl \
-      --runtime-endpoint "$CRI_SOCKET" \
-	  inspect \
+      --runtime-endpoint "$cri_socket" \
+      inspect \
       --output yaml \
-     "$CRI_ID" \
+     "$cri_id" \
   | sed --quiet --expression '/ \+/p' \
   | sed --quiet --expression '1s/^\( \+\).*/\1/p'
 )
-PID=$(
-  chroot $HOSTFS \
+pid=$(
+  chroot $hostfs \
     crictl \
-      --runtime-endpoint "$CRI_SOCKET" \
-	  inspect \
+      --runtime-endpoint "$cri_socket" \
+      inspect \
       --output yaml \
-      "$CRI_ID" \
-  | sed --quiet --expression "/^${INDENT}pid:.*/s/${INDENT}pid: \([[:digit:]]\+\)/\1/p"
+      "$cri_id" \
+  | sed --quiet --expression "/^${indent}pid:.*/s/${indent}pid: \([[:digit:]]\+\)/\1/p"
 )
-if [ -z "$PID" ]; then
+if [ -z "$pid" ]; then
   # assume dockershim runtime, cri id equals to docker container id
-  PID=$(chroot $HOSTFS docker inspect --format '{{.State.Pid}}' "$CRI_ID")
+  pid=$(chroot $hostfs docker inspect --format '{{.State.Pid}}' "$cri_id")
 fi
-if [ -z "$PID" ]; then
-  echo Failed to find the process PID for pod with CRI_ID=$CRI_ID
+if [ -z "$pid" ]; then
+  echo Failed to find the process PID for pod with cri_id=$cri_id
   exit 1
 fi
 
 mkdir -p /kubectl-debugpod
 cat >/kubectl-debugpod/print_root.sh <<EOF
 #!/bin/sh
-HOSTFS=$HOSTFS
-CRI_ID=$CRI_ID
-CRI_PROVIDER=$CRI_PROVIDER
-if [ \$CRI_PROVIDER = docker ]; then
-  ROOT=\$(chroot \$HOSTFS docker inspect --format '{{.GraphDriver.Data.MergedDir}}' "\$CRI_ID")
-elif [ \$CRI_PROVIDER = containerd ]; then
-  ROOT=/run/containerd/io.containerd.runtime.v1.linux/k8s.io/\$CRI_ID/rootfs
-elif [ \$CRI_PROVIDER = cri-o ]; then
-  ROOT=\$(chroot \$HOSTFS runc state \$CRI_ID | sed --quiet --expression 's/ \+"rootfs": "\(.*\)".*/\1/p')
+hostfs=$hostfs
+cri_id=$cri_id
+cri_provider=$cri_provider
+if [ \$cri_provider = docker ]; then
+  root=\$(chroot \$hostfs docker inspect --format '{{.GraphDriver.Data.MergedDir}}' "\$cri_id")
+elif [ \$cri_provider = containerd ]; then
+  root=/run/containerd/io.containerd.runtime.v1.linux/k8s.io/\$cri_id/rootfs
+elif [ \$cri_provider = cri-o ]; then
+  root=\$(chroot \$hostfs runc state \$cri_id | sed --quiet --expression 's/ \+"rootfs": "\(.*\)".*/\1/p')
 fi
-if [ -z "\$ROOT" -o ! -d "\$HOSTFS\$ROOT" ]; then
+if [ -z "\$root" -o ! -d "\$hostfs\$root" ]; then
   echo Failed to obtain the root directory
   exit 1
 fi
-echo \$HOSTFS\$ROOT
+echo \$hostfs\$root
 EOF
 chmod 755 /kubectl-debugpod/print_root.sh
 
@@ -78,5 +78,5 @@ exec nsenter \
   --pid \
   --cgroup \
   --no-fork \
-  --target $PID \
+  --target $pid \
   /bin/sh -c 'mount -t proc proc /proc || true; exec "$@"' /bin/sh "${exec_args[@]}"
